35abf44642df2736d3986c1f5d18913a0a6ba36c
vsheva <valera.sheva@gmail.com>
2023-11-18 21:23:37 -0500

    buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,23 @@ export class AppComponent {
   oddNumbers: number[] = [];
   evenNumbers: number[] = [];
 
-  onIntervalFired(firedNumber: number) {
-    if (firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-    } else {
-      this.oddNumbers.push(firedNumber);
-    }
-  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+// onIntervalFired(firedNumber: number) {
+//   if (firedNumber % 2 === 0) {
+//     this.evenNumbers.push(firedNumber);
+//   } else {
+//     this.oddNumbers.push(firedNumber);
+//   }
+// }
